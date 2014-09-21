b7ce969fe7f55e9fa7d003d376074300b00ad9f5
Dmitriy Tkachenko <wildspirit1994@gmail.com>
2014-09-21 20:00:54 +0300

    Fixed service layer architecture.

--- src/main/java/scheduleApp/InstructorService.java
+++ src/main/java/scheduleApp/InstructorService.java
@@ -1,24 +1,8 @@
 package scheduleApp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class InstructorService {
-    @Autowired
-    private InstructorRepository instructorRepository;
-
-    public List<Instructor> getInstructorAsList(Integer id) {
-        List<Instructor> list = new ArrayList<>();
-        Instructor instructor = instructorRepository.findOne(id);
-        list.add(instructor);
-        return list;
-    }
-
-    public String getInstructorName(Integer id) {
-        return instructorRepository.findOne(id).getName();
-    }
+public interface InstructorService {
+    List<Instructor> getInstructorAsList(Integer id);
+    String getInstructorName(Integer id);
 }
